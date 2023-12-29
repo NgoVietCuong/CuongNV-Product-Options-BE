c871de5ffe54c9b23761554b125681c620147fd4
NgoVietCuong <ngocuong972000@gmail.com>
2023-12-30 00:52:59 +0700

    update option model

--- models/option.js
+++ models/option.js
@@ -4,12 +4,13 @@ const { Schema } = mongoose;
 const optionSchema = new Schema({
   label: { type: String, required: true },
   type: { type: Number, default: 0, enum: [0, 1, 2, 3, 4, 5, 6] },
+  order: { type: Number, required: true },
   textBox: { priceAddOn: Number },
   numberField: { priceAddOn: Number },
-  checkBox: [{ _id: false, optionValue: { type: String }, priceAddOn: { type: Number, default: null } }],
+  checkbox: [{ _id: false, optionValue: { type: String }, priceAddOn: { type: Number, default: null } }],
   radioButton: [{ _id: false, optionValue: { type: String }, priceAddOn: { type: Number, default: null } }],
   dropdownMenu: [{ _id: false, optionValue: { type: String }, priceAddOn: { type: Number, default: null } }],
-  // swatch: [{ swatchType: { type: Number, default: 0, enum: [0, 1]}, optionValue: { type: String, required: true }, priceAddOn: { type: Number, default: null } }],
+  // swatch: [{ _id: false, swatchType: { type: Number, default: 0, enum: [0, 1]}, optionValue: { type: String, required: true }, priceAddOn: { type: Number, default: null } }],
   button: [{ _id: false, optionValue: { type: String }, priceAddOn: { type: Number, default: null } }],
 }, {
   timestamps: false,
